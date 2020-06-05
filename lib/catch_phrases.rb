ceda23e093da1fa399c6b7cab6554f6b4f8cd5c8
Seth Starr <starrseth@hotmail.com>
2020-06-05 17:39:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
+all_phrases = "mario", "toadstool", "link"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def link 
+  puts phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+
